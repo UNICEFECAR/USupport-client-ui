90c017c4f3cecd110c8ef8c50b01deb174f97a28
Georgi 7DIGIT <georgi@7digit.io>
2022-10-24 22:55:39 +0300

    Refactor: the Page block to accept multiple props to render a desired header

--- src/blocks/Page/Page.jsx
+++ src/blocks/Page/Page.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import {
   Navbar,
   CircleIconButton,
   Footer,
+  Icon,
+  Button,
 } from "@USupport-components-library/src";
 import classNames from "classnames";
 import { useTranslation } from "react-i18next";
@@ -17,7 +20,18 @@ import "./page.scss";
  *
  * @return {jsx}
  */
-export const Page = ({ additionalPadding = true, classes, children }) => {
+export const Page = ({
+  additionalPadding,
+  showNavbar,
+  showFooter,
+  showGoBackArrow,
+  showEmergencyButton,
+  heading,
+  subheading,
+  headingButton,
+  classes,
+  children,
+}) => {
   const navigateTo = useNavigate();
   const { t } = useTranslation("page");
   const pages = [
@@ -71,13 +85,15 @@ export const Page = ({ additionalPadding = true, classes, children }) => {
 
   return (
     <>
-      <Navbar
-        pages={pages}
-        countries={countries}
-        dropdownText={t("dropdown_text")}
-        showProfile
-        yourProfileText={t("your_profile_text")}
-      />
+      {showNavbar && (
+        <Navbar
+          pages={pages}
+          countries={countries}
+          dropdownText={t("dropdown_text")}
+          showProfile
+          yourProfileText={t("your_profile_text")}
+        />
+      )}
       <div
         className={[
           "page",
@@ -85,15 +101,90 @@ export const Page = ({ additionalPadding = true, classes, children }) => {
           `${classNames(classes)}`,
         ].join(" ")}
       >
+        <div className="page__header">
+          {showGoBackArrow && (
+            <Icon
+              classes="page__header-icon"
+              name="arrow-chevron-back"
+              size="md"
+              color="#20809E"
+            />
+          )}
+          <h3 className="page__header-heading">{heading}</h3>
+          {headingButton && headingButton}
+        </div>
+        <p className="page__subheading-text text">{subheading}</p>
         {children}
       </div>
-      <CircleIconButton
-        iconName="phone-emergency"
-        classes="page__emergency-button"
-        onClick={() => navigateTo("/sos-center")}
-        label={t("emergency_button")}
-      />
-      <Footer lists={footerLists} contactUsText={t("contact_us")} />
+      {showEmergencyButton && (
+        <CircleIconButton
+          iconName="phone-emergency"
+          classes="page__emergency-button"
+          onClick={() => navigateTo("/sos-center")}
+          label={t("emergency_button")}
+        />
+      )}
+      {showFooter && (
+        <Footer lists={footerLists} contactUsText={t("contact_us")} />
+      )}
     </>
   );
+};
+
+Page.propTypes = {
+  /**
+   * Additional padding on top of the page
+   */
+  additionalPadding: PropTypes.bool,
+
+  /**
+   * Show the navbar
+   */
+  showNavbar: PropTypes.bool,
+
+  /**
+   * Show the footer
+   */
+  showFooter: PropTypes.bool,
+
+  /**
+   * Show the go back arrow
+   */
+  showGoBackArrow: PropTypes.bool,
+
+  /**
+   * Show the emergency button
+   */
+  showEmergencyButton: PropTypes.bool,
+
+  /**
+   * Heading text
+   */
+  heading: PropTypes.string,
+
+  /**
+   * Subheading text
+   */
+  subheading: PropTypes.string,
+
+  /**
+   * Heading button
+   */
+  headingButton: PropTypes.node,
+
+  /**
+   * Additional classes
+   */
+  classes: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
+};
+
+Page.defaultProps = {
+  additionalPadding: true,
+  showNavbar: true,
+  showFooter: true,
+  showGoBackArrow: true,
+  showEmergencyButton: true,
 };
