d4ed6aa277fb1b38dabb45b3c90d41ebe4b145da
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-10-26 18:20:39 +0300

    Create: Login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
 import { SOSCenter } from "./pages/SOSCenter";
+import { Login } from "./pages/Login/Login";
 
 import "./App.scss";
 
@@ -47,6 +48,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/sos-center" element={<SOSCenter contacts={contacts} />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
