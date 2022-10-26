37b61e8fa8e8c14a9ad2cf9c00cc472b25513797
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-10-26 18:20:10 +0300

    Create: Login block

--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,24 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import { Page, SosCenter } from "./blocks/locales.js";
+import { Page, SosCenter, Login } from "./blocks/locales.js";
 
-import { NotFoundPage, SOSCenterPage } from "./pages/locales.js";
+import {
+  NotFoundPage,
+  SOSCenterPage,
+  Login as LoginPage,
+} from "./pages/locales.js";
 
 const resources = {
   en: {
     page: Page.en,
     "sos-center": SosCenter.en,
+    login: Login.en,
 
     // Pages
     "not-found-page": NotFoundPage.en,
     "sos-center-page": SOSCenterPage.en,
+    "login-page": LoginPage.en,
   },
 };
 
