d627bef16c2cb45cc0394b86cac704993e855d3c
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-10-27 17:54:56 +0300

    Create: RegisterAboutYou block

---
+++ src/blocks/RegisterAboutYou/RegisterAboutYou.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import {
+  Block,
+  Input,
+  DropdownWithLabel,
+  RadioButtonSelectorGroup,
+  Button,
+  Grid,
+  GridItem,
+} from "@USupport-components-library/src";
+import { useTranslation } from "react-i18next";
+
+import "./register-about-you.scss";
+
+/**
+ * RegisterAboutYou
+ *
+ * RegisterAboutYou block
+ *
+ * @return {jsx}
+ */
+export const RegisterAboutYou = () => {
+  const { t } = useTranslation("register-about-you");
+
+  const [data, setData] = React.useState({
+    name: "",
+    gender: null,
+    yearOfBirth: null,
+    placeOfLiving: null,
+  });
+
+  const genderOptions = [
+    { label: t("gender_male"), value: "male" },
+    { label: t("gender_female"), value: "female" },
+  ];
+
+  const placeOfLivingOptions = [
+    { label: t("place_of_living_urban"), value: "Urban" },
+    { label: t("place_of_living_rural"), value: "Rural" },
+  ];
+
+  const yearsOptions = () => {
+    const currentYear = new Date().getFullYear();
+    const years = [];
+    for (let i = 0; i < 100; i++) {
+      years.push({ label: `${currentYear - i}`, value: `${currentYear - i}` });
+    }
+    return years;
+  };
+
+  const handleSelect = (field, value) => {
+    const newData = { ...data };
+
+    newData[field] = value;
+
+    setData(newData);
+  };
+
+  const handleContinue = () => {
+    console.log("Continue");
+  };
+
+  return (
+    <Block classes="register-about-you">
+      <Grid md={8} lg={12} classes="register-about-you__grid">
+        <GridItem
+          md={8}
+          lg={12}
+          classes="register-about-you__grid__content-item"
+        >
+          <div className="register-about-you__grid__content-item__inputs-container">
+            <Input
+              label={t("input_nickname_label")}
+              placeholder={t("input_nickname_placeholder")}
+              name="nickname"
+              onChange={(e) => handleSelect("name", e.target.value)}
+            />
+            <DropdownWithLabel
+              options={genderOptions}
+              selected={data.gender}
+              setSelected={(option) => handleSelect("gender", option)}
+              label={t("dropdown_gender_label")}
+              classes="register-about-you__grid__content-item__inputs-container__gender-dropdown"
+            />
+            <DropdownWithLabel
+              options={yearsOptions()}
+              selected={data.yearOfBirth}
+              setSelected={(option) => handleSelect("yearOfBirth", option)}
+              label={t("dropdown_year_label")}
+              classes="register-about-you__grid__content-item__inputs-container__year-dropdown"
+            />
+            <RadioButtonSelectorGroup
+              name="placeOfLiving"
+              label={t("radio_button_selector_group_label")}
+              options={placeOfLivingOptions}
+              selected={data.placeOfLiving}
+              setSelected={(option) => handleSelect("placeOfLiving", option)}
+            />
+          </div>
+          <Button size="lg" label="Continue" onClick={() => handleContinue()} />
+        </GridItem>
+      </Grid>
+    </Block>
+  );
+};
--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,24 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import { Page, SosCenter } from "./blocks/locales.js";
+import { Page, SosCenter, RegisterAboutYou } from "./blocks/locales.js";
 
-import { NotFoundPage, SOSCenterPage } from "./pages/locales.js";
+import {
+  NotFoundPage,
+  SOSCenterPage,
+  RegisterAboutYou as RegisterAboutYouPage,
+} from "./pages/locales.js";
 
 const resources = {
   en: {
     page: Page.en,
     "sos-center": SosCenter.en,
+    "register-about-you": RegisterAboutYou.en,
 
     // Pages
     "not-found-page": NotFoundPage.en,
     "sos-center-page": SOSCenterPage.en,
+    "register-about-you-page": RegisterAboutYouPage.en,
   },
 };
 
