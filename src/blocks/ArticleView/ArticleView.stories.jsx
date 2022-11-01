790cd6897018c252d920f87abc6c7106bba70aa8
Sebastian Galustyan <sebastian@7digit.io>
2022-11-01 20:37:32 +0000

    Fix: stories for Articles and ArticleView blocks

--- src/blocks/ArticleView/ArticleView.stories.jsx
+++ src/blocks/ArticleView/ArticleView.stories.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { ArticleView } from './ArticleView';
+import { ArticleView } from "./ArticleView";
 
 export default {
-  title: 'Website UI/blocks/ArticleView',
+  title: "Client UI/blocks/ArticleView",
   component: ArticleView,
   argTypes: {},
 };
 
-const Template = (props) => <ArticleView {...props} />;
+const articleData = {
+  title: "Article Title",
+  creator: "Article Creator",
+  readingTime: "5",
+  body: "Article Body",
+  labels: [
+    {
+      name: "Label 1",
+    },
+
+    {
+      name: "Label 2",
+    },
+  ],
+};
+
+const Template = (props) => (
+  <ArticleView articleData={articleData} {...props} />
+);
 
 export const Default = Template.bind({});
 Default.args = {};
--- src/blocks/Articles/Articles.stories.jsx
+++ src/blocks/Articles/Articles.stories.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-
-import { Articles } from './Articles';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Articles } from "./Articles";
 
 export default {
-  title: 'Website UI/blocks/Articles',
+  title: "Client UI/blocks/Articles",
   component: Articles,
   argTypes: {},
 };
 
-const Template = (props) => <Articles {...props} />;
+// Create a react-query client
+const queryClient = new QueryClient();
+
+const Template = (props) => (
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <Articles {...props} />
+    </Router>
+  </QueryClientProvider>
+);
 
 export const Default = Template.bind({});
 Default.args = {};
