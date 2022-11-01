20149f985c74b7e4a9c8af5c5315108ca0cf3094
Sebastian Galustyan <sebastian@7digit.io>
2022-11-01 20:03:25 +0000

    Add: Initialise QuerryClient and create new routes for the information portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   NotFound,
   SOSCenter,
@@ -8,13 +9,19 @@ import {
   NotificationPreferencesPage,
   Login,
   Welcome,
+  InformationPortal,
+  Articles,
+  ArticleInformation,
 } from "#pages";
-
-import "./App.scss";
 
 // AOS imports
 import "aos/dist/aos.css";
 import AOS from "aos";
+
+import "./App.scss";
+
+// Create a react-query client
+const queryClient = new QueryClient();
 
 function App() {
   // TODO: add the country specific information about the SOS center
@@ -51,20 +58,28 @@ function App() {
   });
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/sos-center" element={<SOSCenter contacts={contacts} />} />
-        <Route path="/register" element={<RegisterAboutYou />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route
-          path="/settings/notifications"
-          element={<NotificationPreferencesPage />}
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route
+            path="/sos-center"
+            element={<SOSCenter contacts={contacts} />}
+          />
+          <Route path="/register" element={<RegisterAboutYou />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route
+            path="/settings/notifications"
+            element={<NotificationPreferencesPage />}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/information-portal" element={<InformationPortal />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/article/:id" element={<ArticleInformation />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
