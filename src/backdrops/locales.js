159dd148c41ab026d320dbf52f5b8f57e82911a3
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-11-05 00:02:34 +0200

    Create: ConfirmConsultation backdrop

--- src/backdrops/locales.js
+++ src/backdrops/locales.js
@@ -1,2 +1,3 @@
 export * as JoinConsultation from "./JoinConsultation/locales.js";
 export * as FilterProviders from './FilterProviders/locales.js';
+export * as ConfirmConsultation from './ConfirmConsultation/locales.js';
