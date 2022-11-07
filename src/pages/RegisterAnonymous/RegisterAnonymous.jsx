ccccdf941ccfa206a2fd1b3cf447cb2de116942d
Georgi 7DIGIT <georgi@7digit.io>
2022-11-07 04:27:35 +0200

    Refactor: handle already logged in user redirection

--- src/pages/RegisterAnonymous/RegisterAnonymous.jsx
+++ src/pages/RegisterAnonymous/RegisterAnonymous.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import { Page, RegisterAnonymous as RegisterAnonymousBlock } from "#blocks";
-import { RadialCircle } from "@USupport-components-library/src";
+import { RadialCircle, Loading } from "@USupport-components-library/src";
 import { useWindowDimensions } from "@USupport-components-library/utils";
+import { useIsLoggedIn } from "@USupport-components-library/hooks";
 
 import "./register-anonymous.scss";
 
@@ -18,16 +19,18 @@ export const RegisterAnonymous = () => {
   const { t } = useTranslation("register-anonymous-page");
   const { width } = useWindowDimensions();
   const navigate = useNavigate();
+  const isLoggedIn = useIsLoggedIn();
 
   const handleGoBack = () => {
     navigate("/register-preview");
   };
 
+  if (isLoggedIn === "loading") return <Loading />;
+  if (isLoggedIn === true) return <Navigate to="/dashboard" />;
+
   return (
     <Page
       classes="page__register-anonymous"
-      showFooter={false}
-      showNavbar={false}
       additionalPadding={false}
       heading={t("heading")}
       handleGoBack={handleGoBack}
