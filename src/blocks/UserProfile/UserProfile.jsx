77f1d6aac914fbc893f126a79cb261e85099b658
Georgi 7DIGIT <georgi@7digit.io>
2022-11-12 23:27:21 +0200

    Add: API connection to the UserProfile

--- src/blocks/UserProfile/UserProfile.jsx
+++ src/blocks/UserProfile/UserProfile.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import {
   Block,
   Grid,
   GridItem,
   ButtonSelector,
-  Loading,
 } from "@USupport-components-library/src";
-import { useTranslation } from "react-i18next";
-
+import { useGetClientData } from "#hooks";
 import "./user-profile.scss";
+
+const AMAZON_S3_BUCKET = `${import.meta.env.VITE_AMAZON_S3_BUCKET}`;
 
 /**
  * UserProfile
@@ -17,11 +19,34 @@ import "./user-profile.scss";
  *
  * @return {jsx}
  */
-export const UserProfile = () => {
+export const UserProfile = ({ openModal, closeModal, isTmpUser }) => {
+  const navigate = useNavigate();
   const { t } = useTranslation("user-profile");
 
+  const [displayName, setDisplayName] = useState("");
+
+  const clientData = useGetClientData()[1];
+
+  useEffect(() => {
+    if (clientData) {
+      if (clientData.name && clientData.surname) {
+        setDisplayName(`${clientData.name} ${clientData.surname}`);
+      } else {
+        setDisplayName(clientData.nickname);
+      }
+    }
+  }, [clientData]);
+
   const handleRedirect = (redirectTo) => {
-    console.log(`Redirect to: ${redirectTo}`);
+    if (
+      (redirectTo === "/details" ||
+        redirectTo === "/notification-preferences") &&
+      isTmpUser
+    ) {
+      openModal();
+    } else {
+      navigate(`/${redirectTo}`);
+    }
   };
 
   return (
@@ -32,9 +57,10 @@ export const UserProfile = () => {
             {t("your_profile")}
           </p>
           <ButtonSelector
-            label="Anna Frank"
+            label={displayName || t("guest")}
             classes="user-profile__grid__item__button "
-            onClick={() => handleRedirect("/profile")}
+            onClick={() => handleRedirect("/details")}
+            avatar={`${AMAZON_S3_BUCKET}/${clientData?.image || "default"}`}
           />
         </GridItem>
         <GridItem md={8} lg={12} classes="user-profile__grid__item">
@@ -42,16 +68,10 @@ export const UserProfile = () => {
             {t("application_settings")}
           </p>
           <ButtonSelector
-            label={t("passcode_biometrics_button_label")}
-            iconName="fingerprint"
-            classes="user-profile__grid__item__button"
-            onClick={() => handleRedirect("/passcode-and-biometrics")}
-          />
-          <ButtonSelector
             label={t("notifications_settings_button_label")}
             iconName="notifications"
             classes="user-profile__grid__item__button"
-            onClick={() => handleRedirect("/notifications-settings")}
+            onClick={() => handleRedirect("/notification-preferences")}
           />
         </GridItem>
         <GridItem md={8} lg={12} classes="user-profile__grid__item">
