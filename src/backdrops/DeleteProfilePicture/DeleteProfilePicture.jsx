2d9cb65d6b5fe859b2897aed094cce994c20964a
Georgi 7DIGIT <georgi@7digit.io>
2022-11-12 23:36:50 +0200

    Add: API connection in DeleteProfilePicture backdrop

--- src/backdrops/DeleteProfilePicture/DeleteProfilePicture.jsx
+++ src/backdrops/DeleteProfilePicture/DeleteProfilePicture.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Backdrop } from "@USupport-components-library/src";
 import { useError } from "@USupport-components-library/hooks";
+import { clientSvc } from "@USupport-components-library/services";
 
 import "./delete-profile-picture.scss";
 
@@ -18,10 +19,17 @@ export const DeleteProfilePicture = ({ isOpen, onClose }) => {
   const [error, setError] = useState();
   const queryClient = useQueryClient();
 
-  const deletePicture = async () => {};
+  const deletePicture = async () => {
+    const res = await clientSvc.deleteImage();
+    if (res.status === 200) {
+      return true;
+    }
+  };
+
   const deletePictureMutation = useMutation(deletePicture, {
     onSuccess: () => {
       queryClient.invalidateQueries(["client-data"]);
+      onClose();
     },
     onError: (error) => {
       const { message: errorMessage } = useError(error);
