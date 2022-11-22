cb1bb1d3b9ad557a72c111b540f6a25217310528
Georgi 7DIGIT <georgi@7digit.io>
2022-11-22 23:51:38 +0200

    Refactor: the ProviderOverview page to be able to handle booking of consultations

--- src/pages/ProviderOverview/ProviderOverview.jsx
+++ src/pages/ProviderOverview/ProviderOverview.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useLocation, Navigate } from "react-router-dom";
 import { Button, RadialCircle } from "@USupport-components-library/src";
+import {
+  useBlockSlot,
+  useScheduleConsultation,
+} from "@USupport-components-library/hooks";
 import { Page, ProviderOverview as ProviderOverviewBlock } from "#blocks";
+import { SelectConsultation, ConfirmConsultation } from "#backdrops";
 
 import "./provider-overview.scss";
 
@@ -14,9 +20,67 @@ import "./provider-overview.scss";
  */
 export const ProviderOverview = () => {
   const { t } = useTranslation("provider-overview-page");
+  // Should we get the provider ID from the URL or from the location
+  const location = useLocation();
+  const providerData = location.state?.providerData;
+  if (!providerData) return <Navigate to="/select-provider" />;
 
-  const handleSchedule = () => {
-    console.log("schedule");
+  const [isBlockSlotSubmitting, setIsBlockSlotSubmitting] = useState(false);
+  const [blockSlotError, setBlockSlotError] = useState();
+  const [consultationId, setConsultationId] = useState();
+  const [selectedSlot, setSelectedSlot] = useState();
+
+  // Modal state variables
+  const [isScheduleBackdropOpen, setIsScheduleBackdropOpen] = useState(false);
+  const [isConfirmBackdropOpen, setIsConfirmBackdropOpen] = useState(false);
+
+  // Open modals
+  const openScheduleBackdrop = () => setIsScheduleBackdropOpen(true);
+  const openConfirmConsultationBackdrop = () => setIsConfirmBackdropOpen(true);
+
+  // Close modals
+  const closeConfirmConsultationBackdrop = () =>
+    setIsConfirmBackdropOpen(false);
+  const closeScheduleBackdrop = () => setIsScheduleBackdropOpen(false);
+
+  const onBlockSlotSuccess = (consultationId) => {
+    // setIsBlockSlotSubmitting(false);
+    // setConsultationId(consultationId);
+
+    scheduleConsultationMutation.mutate(consultationId);
+
+    // closeScheduleBackdrop();
+    // openConfirmConsultationBackdrop();
+  };
+  const onBlockSlotError = (error) => {
+    setBlockSlotError(error);
+    setIsBlockSlotSubmitting(false);
+  };
+  const blockSlotMutation = useBlockSlot(onBlockSlotSuccess, onBlockSlotError);
+
+  const onScheduleConsultationSuccess = (data) => {
+    setIsBlockSlotSubmitting(false);
+    setConsultationId(consultationId);
+    closeScheduleBackdrop();
+    openConfirmConsultationBackdrop();
+    setBlockSlotError(null);
+  };
+  const onScheduleConsultationError = (error) => {
+    setBlockSlotError(error);
+    setIsBlockSlotSubmitting(false);
+  };
+  const scheduleConsultationMutation = useScheduleConsultation(
+    onScheduleConsultationSuccess,
+    onScheduleConsultationError
+  );
+
+  const handleBlockSlot = (slot) => {
+    setIsBlockSlotSubmitting(true);
+    setSelectedSlot(slot);
+    blockSlotMutation.mutate({
+      slot,
+      providerId: providerData.providerDetailId,
+    });
   };
 
   return (
@@ -25,18 +89,38 @@ export const ProviderOverview = () => {
       heading={t("heading")}
       subheading={t("subheading")}
     >
-      <ProviderOverviewBlock />
-      <div className="page__provider-overview__button-container">
-        <Button
-          label={t("button_label")}
-          size="lg"
-          onClick={() => handleSchedule()}
+      {providerData && (
+        <ProviderOverviewBlock
+          provider={providerData}
+          openScheduleBackdrop={openScheduleBackdrop}
         />
-      </div>
+      )}
       <RadialCircle
         color="purple"
         classes="page__provider-overview__radial-circle"
       />
+      <SelectConsultation
+        isOpen={isScheduleBackdropOpen}
+        onClose={closeScheduleBackdrop}
+        handleBlockSlot={handleBlockSlot}
+        providerId={providerData?.providerDetailId}
+        isCtaDisabled={isBlockSlotSubmitting}
+        errorMessage={blockSlotError}
+      />
+      {selectedSlot && (
+        <ConfirmConsultation
+          isOpen={isConfirmBackdropOpen}
+          onClose={closeConfirmConsultationBackdrop}
+          consultation={{
+            startDate: new Date(selectedSlot),
+            endDate: new Date(
+              new Date(selectedSlot).setHours(
+                new Date(selectedSlot).getHours() + 1
+              )
+            ),
+          }}
+        />
+      )}
     </Page>
   );
 };
