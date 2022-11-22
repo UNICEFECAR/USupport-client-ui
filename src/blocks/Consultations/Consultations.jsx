b9f472e1c36e4032dbf6828640d8e24c6eefb77a
Georgi 7DIGIT <georgi@7digit.io>
2022-11-22 23:55:12 +0200

    Add: placeholder data in Consultations block and event handlers for different actions(edit,join, open details)

--- src/blocks/Consultations/Consultations.jsx
+++ src/blocks/Consultations/Consultations.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useCallback, useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
 import {
   Block,
   Grid,
@@ -18,7 +20,11 @@ import "./consultations.scss";
  *
  * @return {jsx}
  */
-export const Consultations = ({}) => {
+export const Consultations = ({
+  openEditConsultation,
+  openJoinConsultation,
+}) => {
+  const navigate = useNavigate();
   let today = new Date();
   const { t } = useTranslation("consultations");
 
@@ -29,29 +35,51 @@ export const Consultations = ({}) => {
 
   const [filter, setFilter] = useState("upcoming");
 
-  const fetchConsultations = async () => {};
+  const daysOfWeekTranslations = {
+    monday: t("monday"),
+    tuesday: t("tuesday"),
+    wednesday: t("wednesday"),
+    thursday: t("thursday"),
+    friday: t("friday"),
+    saturday: t("saturday"),
+    sunday: t("sunday"),
+  };
 
+  const fetchConsultations = async () => {};
   const consultationsQuery = useQuery(["consultations"], fetchConsultations, {
     enabled: false, // TODO: Enable this when the API is ready and remove the placeholder data
-    placeholderData: [1, 2, 3, 4, 5, 6, 7, 8, 9].map((x) => {
-      return {
-        id: x,
-        specialistName: "Dr. Joanna Doe " + x.toString(),
-        startDate:
-          x < 4
-            ? new Date("2022-11-1 15:00")
-            : x === 4
-            ? new Date(today.setHours(today.getHours() - 1))
-            : new Date(today.setDate(today.getDate() + 3)),
-        endDate:
-          x < 4
-            ? new Date("2022-11-1 16:00")
-            : x === 4
-            ? new Date(today.setHours(today.getHours() + 4))
-            : new Date(today.setDate(today.getDate() + 3)),
+    placeholderData: [
+      {
+        id: 1,
+        specialistName: "Dr. Joanna doe 1",
+        timestamp: 1668699720000,
         overview: false,
-      };
-    }),
+      },
+      {
+        id: 2,
+        specialistName: "Dr. Joanna doe 2",
+        timestamp: 1668879057000,
+        overview: false,
+      },
+      {
+        id: 3,
+        specialistName: "Dr. Joanna doe 3",
+        timestamp: 1668886182290,
+        overview: false,
+      },
+      {
+        id: 4,
+        specialistName: "Dr. Joanna doe 4",
+        timestamp: 1669141319000,
+        overview: false,
+      },
+      {
+        id: 5,
+        specialistName: "Dr. Joanna doe 5",
+        timestamp: 1669832519000,
+        overview: false,
+      },
+    ],
   });
 
   const handleTabClick = (index) => {
@@ -69,18 +97,33 @@ export const Consultations = ({}) => {
     setFilter(optionsCopy[index].value);
   };
 
+  const handleOpenEdit = (providerId) => {
+    openEditConsultation(providerId);
+  };
+
+  const handleOpenDetails = (providerId, consultationId) => {
+    // TODO: Redirect to the activity history page
+    navigate("/activity-history", {
+      state: {
+        providerId,
+        consultationId,
+      },
+    });
+  };
+
   const filterConsultations = useCallback(() => {
     const currentDate = new Date();
+    const currentDateTs = currentDate.getTime();
 
     return consultationsQuery.data?.filter((consultation) => {
+      const endTime = consultation.timestamp + 3600000;
       if (filter === "upcoming") {
         return (
-          consultation.startDate >= currentDate ||
-          (currentDate >= consultation.startDate &&
-            currentDate <= consultation.endDate)
+          consultation.timestamp >= currentDateTs ||
+          (currentDateTs >= consultation.timestamp && currentDateTs <= endTime)
         );
       } else {
-        return consultation.endDate < currentDate;
+        return endTime < currentDateTs;
       }
     });
   }, [consultationsQuery.data, filter]);
@@ -88,12 +131,19 @@ export const Consultations = ({}) => {
   const renderAllConsultations = useMemo(() => {
     const filteredConsultations = filterConsultations();
 
+    if (filteredConsultations?.length === 0)
+      return (
+        <GridItem md={8} lg={12}>
+          <p>{t("no_consultations")}</p>
+        </GridItem>
+      );
     return filteredConsultations.map((consultation, index) => {
+      const hasMoreThanOne = filteredConsultations.length > 1;
       return (
         <GridItem
           key={index}
-          md={4}
-          lg={6}
+          md={hasMoreThanOne ? 4 : 8}
+          lg={hasMoreThanOne ? 6 : 12}
           classes="consultations__grid__consultations-item__grid__consultation"
         >
           <Consultation
@@ -102,6 +152,12 @@ export const Consultations = ({}) => {
             endDate={consultation.endDate}
             overview={consultation.overview}
             renderIn="client"
+            handleOpenEdit={handleOpenEdit}
+            handleJoinClick={openJoinConsultation}
+            handleOpenDetails={handleOpenDetails}
+            timestamp={consultation.timestamp}
+            daysOfWeekTranslations={daysOfWeekTranslations}
+            providerId={consultation.id.toString()}
           />
         </GridItem>
       );
@@ -111,7 +167,7 @@ export const Consultations = ({}) => {
   return (
     <Block classes="consultations">
       <Grid md={8} lg={12} classes="consultations__grid">
-        <GridItem md={4} lg={6} classes="consultations__grid__tabs-item">
+        <GridItem md={8} lg={12} classes="consultations__grid__tabs-item">
           <TabsUnderlined options={tabsOptions} handleSelect={handleTabClick} />
         </GridItem>
         <GridItem
