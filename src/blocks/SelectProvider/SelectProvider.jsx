38ebb80d4ad7d6c63670d53e3617908df16da090
Georgi 7DIGIT <georgi@7digit.io>
2022-11-22 23:49:34 +0200

    Add: display text when there are no providers found in SelectProvider block

--- src/blocks/SelectProvider/SelectProvider.jsx
+++ src/blocks/SelectProvider/SelectProvider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useTranslation } from "react-i18next";
 import {
   Block,
   Grid,
@@ -18,6 +18,7 @@ import "./select-provider.scss";
  * @return {jsx}
  */
 export const SelectProvider = ({ providers }) => {
+  const { t } = useTranslation("select-provider");
   const navigate = useNavigate();
 
   const handleProviderClick = (provider) => {
@@ -29,20 +30,26 @@ export const SelectProvider = ({ providers }) => {
   };
 
   const renderProviders = () => {
+    if (providers?.length === 0)
+      return (
+        <GridItem classes="select-provider__grid__item-no-match" md={8} lg={12}>
+          <p>{t("no_match")}</p>
+        </GridItem>
+      );
     return providers?.map((provider) => {
       return (
         <GridItem
           md={4}
           lg={6}
-          key={provider.id}
+          key={provider.providerDetailId}
           classes="select-provider__grid__provider"
         >
           <ProviderOverview
+            provider={provider}
             name={provider.name}
             patronym={provider.patronym}
             surname={provider.surname}
-            types={provider.types}
-            experience={provider.experience}
+            specializations={provider.specializations.map((x) => t(x))}
             onClick={() => handleProviderClick(provider)}
             image={provider.image}
           />
