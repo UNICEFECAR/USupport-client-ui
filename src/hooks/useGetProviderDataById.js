e37b56cf62d0b1c2795e2bbd14a85ad9e04e127b
Georgi 7DIGIT <georgi@7digit.io>
2022-11-27 01:56:07 +0200

    Create: useGetProviderDataById hook

---
+++ src/hooks/useGetProviderDataById.js
@@ -0,0 +1,46 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { providerSvc } from "@USupport-components-library/services";
+
+/**
+ * Reuseable hook to get and transform the provider data in a desired format
+ */
+export default function useGetProviderDataById(id) {
+  //   const queryClient = useQueryClient();
+  const fetchProvidersData = async () => {
+    let data;
+
+    const response = await providerSvc.getProviderById(id);
+    data = response.data;
+
+    const formattedData = {
+      providerDetailId: data.provider_detail_id || "",
+      name: data.name || "",
+      patronym: data.patronym || "",
+      surname: data.surname || "",
+      nickname: data.nickname || "",
+      email: data.email || "",
+      phonePrefix: data.phone_prefix || "",
+      phone: data.phone || "",
+      image: data.image || "default",
+      specializations: data.specializations || [],
+      education: data.education || [],
+      sex: data.sex || "",
+      consultationPrice: data.consultation_price || 0,
+      description: data.description || "",
+      languages: data.languages || [],
+      workWith: data.work_with || [],
+      totalConsultations: data.total_consultations || 0,
+      earliestAvailableSlot: data.earliest_available_slot || "",
+    };
+    return formattedData;
+  };
+
+  const providersDataQuery = useQuery(["provider-data"], fetchProvidersData, {
+    onError: (err) => console.log(err, "err"),
+    notifyOnChangeProps: ["data"],
+  });
+
+  return providersDataQuery;
+}
+
+export { useGetProviderDataById };
