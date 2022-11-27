4b493e67a5c020fb0c7f4f8ea8334944cfbd9881
Sebastian Galustyan <sebastian@7digit.io>
2022-11-27 21:33:37 +0000

    Refactor: removed uasage of CMS_HOST env variable

--- src/blocks/Articles/Articles.jsx
+++ src/blocks/Articles/Articles.jsx
@@ -32,8 +32,6 @@ export const Articles = ({
   showAgeGroups,
   sort,
 }) => {
-  const CMS_HOST = `${import.meta.env.VITE_CMS_HOST}`;
-
   const navigate = useNavigate();
   const { i18n, t } = useTranslation("articles");
 
@@ -324,10 +322,7 @@ export const Articles = ({
                 !isArticlesFetching && (
                   <Grid>
                     {articles?.map((article, index) => {
-                      const articleData = destructureArticleData(
-                        CMS_HOST,
-                        article
-                      );
+                      const articleData = destructureArticleData(article);
                       return (
                         <GridItem lg={6} key={index}>
                           <CardMedia
