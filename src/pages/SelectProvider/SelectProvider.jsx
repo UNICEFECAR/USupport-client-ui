1bd41ab16847fe0768975202aafbca273c36c806
Sebastian Galustyan <sebastian@7digit.io>
2022-12-03 04:23:02 +0000

    Fix:  error with searching field in a wrong data entry

--- src/pages/SelectProvider/SelectProvider.jsx
+++ src/pages/SelectProvider/SelectProvider.jsx
@@ -33,7 +33,7 @@ export const SelectProvider = () => {
   const checkProviderHasType = (provider, types) => {
     return types
       .map((x) => {
-        return provider.types.includes(x);
+        return provider.specializations.includes(x);
       })
       .some((x) => x === true);
   };
