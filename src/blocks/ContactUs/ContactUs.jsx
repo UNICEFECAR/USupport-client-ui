6e8786f2b859d999cd5702dd2971d747610b5fe1
Georgi 7DIGIT <georgi@7digit.io>
2023-01-25 00:05:48 +0200

    Refactor: the ContactUs block to add the contact form in the database

--- src/blocks/ContactUs/ContactUs.jsx
+++ src/blocks/ContactUs/ContactUs.jsx
@@ -9,24 +9,20 @@ import {
   GridItem,
   Button,
   Modal,
+  Input,
+  Loading,
 } from "@USupport-components-library/src";
 import { validate } from "@USupport-components-library/utils";
-import { useSendIssueEmail } from "#hooks";
+
+import { useSendIssueEmail, useGetClientData } from "#hooks";
 import Joi from "joi";
 
 import "./contact-us.scss";
 
-const initialIssues = [
-  { label: "Reason 1", value: "reason-1", selected: false },
-  { label: "Reason 2", value: "reason-2", selected: false },
-  { label: "Reason 3", value: "reason-3", selected: false },
-  { label: "Reason 4", value: "reason-4", selected: false },
-  { label: "Reason 5", value: "reason-5", selected: false },
-];
-
 const initialData = {
   issue: null,
   message: "",
+  email: "",
 };
 
 /**
@@ -37,8 +33,17 @@ const initialData = {
  * @return {jsx}
  */
 export const ContactUs = () => {
-  const navigate = useNavigate();
   const { t } = useTranslation("contact-us-block");
+
+  const initialIssues = [
+    { value: "information", label: t("contact_reason_1") },
+    { value: "technical-problem", label: t("contact_reason_2") },
+    { value: "join-as-provider", label: t("contact_reason_3") },
+    { value: "partnerships", label: t("contact_reason_4") },
+    { value: "other", label: t("contact_reason_5") },
+  ];
+
+  const navigate = useNavigate();
   const [data, setData] = useState({ ...initialData });
   const [issues, setIssues] = useState([...initialIssues]);
   const [errors, setErrors] = useState({});
@@ -46,8 +51,24 @@ export const ContactUs = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
 
+  const [clientDataQuery] = useGetClientData();
+
+  useEffect(() => {
+    if (clientDataQuery.data) {
+      const { email } = clientDataQuery.data;
+      console.log(email);
+      setData({
+        ...data,
+        email,
+      });
+    }
+  }, [clientDataQuery.data]);
+
   const schema = Joi.object({
     issue: Joi.string().label(t("issue_error")),
+    email: Joi.string()
+      .email({ tlds: { allow: false } })
+      .label(t("email_error")),
     message: Joi.string().min(5).label(t("message_error")),
   });
 
@@ -108,12 +129,15 @@ export const ContactUs = () => {
       const dataToValidate = {
         issue: data.issue,
         message: data.message,
+        email: data.email,
       };
       if ((await validate(dataToValidate, schema, setErrors)) === null) {
         const payload = {
-          subject: "Technical issue",
+          subjectValue: data.issue,
+          subjectLabel: "Technical issue",
           title: issues.find((x) => x.value === data.issue)?.label,
           text: data.message,
+          email: data.email,
         };
         sendIssueEmailMutation.mutate(payload);
       } else {
@@ -124,46 +148,60 @@ export const ContactUs = () => {
 
   return (
     <Block classes="contact-us">
-      <Grid classes="contact-us__grid" xs={4} md={8} lg={12}>
-        <GridItem xs={4} md={8} lg={12}>
-          <DropdownWithLabel
-            label={t("issue")}
-            errorMessage={errors.issue}
-            classes="contact-us__issue-input"
-            placeholder={t("issue_placeholder")}
-            options={issues}
-            selected={data.issue}
-            setSelected={handleIssueChange}
-          />
-        </GridItem>
-        <GridItem
-          classes="contact-us__grid__textarea-item"
-          xs={4}
-          md={8}
-          lg={12}
-        >
-          <Textarea
-            label={t("message")}
-            placeholder={t("message_placeholder")}
-            onChange={(value) => handleChange("message", value)}
-            errorMessage={errors.message}
-            classes="contact-us__message-input"
-            value={data.message}
-          />
-        </GridItem>
+      {clientDataQuery.isLoading && !clientDataQuery.data ? (
+        <Loading size="lg" />
+      ) : (
+        <Grid classes="contact-us__grid" xs={4} md={8} lg={12}>
+          <GridItem xs={4} md={8} lg={12}>
+            <DropdownWithLabel
+              label={t("issue")}
+              errorMessage={errors.issue}
+              classes="contact-us__issue-input"
+              placeholder={t("issue_placeholder")}
+              options={issues}
+              selected={data.issue}
+              setSelected={handleIssueChange}
+            />
+          </GridItem>
+          {clientDataQuery.data.accessToken && (
+            <GridItem xs={4} md={8} lg={12}>
+              <Input
+                value={data.email}
+                label={t("email")}
+                placeholder="user@mail.com"
+                onChange={(e) => handleChange("email", e.currentTarget.value)}
+              />
+            </GridItem>
+          )}
+          <GridItem
+            classes="contact-us__grid__textarea-item"
+            xs={4}
+            md={8}
+            lg={12}
+          >
+            <Textarea
+              label={t("message")}
+              placeholder={t("message_placeholder")}
+              onChange={(value) => handleChange("message", value)}
+              errorMessage={errors.message}
+              classes="contact-us__message-input"
+              value={data.message}
+            />
+          </GridItem>
 
-        <GridItem xs={4} md={8} lg={12}>
-          <Button
-            classes="contact-us__grid__button"
-            size="lg"
-            label={t("button")}
-            type="primary"
-            color="green"
-            onClick={handleSubmit}
-            disabled={!canSubmit || isSubmitting}
-          />
-        </GridItem>
-      </Grid>
+          <GridItem xs={4} md={8} lg={12}>
+            <Button
+              classes="contact-us__grid__button"
+              size="lg"
+              label={t("button")}
+              type="primary"
+              color="green"
+              onClick={handleSubmit}
+              disabled={!canSubmit || isSubmitting}
+            />
+          </GridItem>
+        </Grid>
+      )}
       <Modal
         isOpen={isSuccessModalOpen}
         closeModal={closeSuccessModal}
