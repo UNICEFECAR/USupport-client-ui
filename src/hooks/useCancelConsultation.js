cba225e20ff5d85f0ac9b1212382f3901739f910
Sebastian Galustyan <sebastian@7digit.io>
2023-01-26 23:44:35 +0000

    Add: functionality to useCancelConsultation and useAcceptConsultation for payments integration

--- src/hooks/useCancelConsultation.js
+++ src/hooks/useCancelConsultation.js
@@ -1,11 +1,23 @@
 import { useMutation } from "@tanstack/react-query";
-import { providerSvc } from "@USupport-components-library/services";
+import {
+  providerSvc,
+  paymentsSvc,
+} from "@USupport-components-library/services";
 import { useError } from "./useError";
 
 export default function useCancelConsultation(onSuccess, onError) {
-  const cancelConsultation = async (consultationId) => {
-    const res = await providerSvc.cancelConsultation(consultationId);
-    return res;
+  const cancelConsultation = async ({
+    consultationId,
+    price,
+    shouldRefund,
+  }) => {
+    if (price > 0 && shouldRefund) {
+      await paymentsSvc.refund(consultationId);
+    } else {
+      console.log("no refund");
+      await providerSvc.cancelConsultation(consultationId);
+    }
+    return true;
   };
 
   const cancelConsultationMutation = useMutation(cancelConsultation, {
