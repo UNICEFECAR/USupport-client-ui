2bc3fb9331385b5121c07c432d8fddb5a97c01e0
Georgi 7DIGIT <georgi@7digit.io>
2023-02-22 03:21:34 +0200

    Fix: small bugs in useIsLoggedIn and RegisterAboutYou

--- src/blocks/RegisterAboutYou/RegisterAboutYou.jsx
+++ src/blocks/RegisterAboutYou/RegisterAboutYou.jsx
@@ -47,7 +47,7 @@ export const RegisterAboutYou = () => {
     { label: t("sex_male"), value: "male" },
     { label: t("sex_female"), value: "female" },
     { label: t("sex_unspecified"), value: "unspecified" },
-    { label: t("sex_none"), value: "none" },
+    { label: t("sex_none"), value: "notMentioned" },
   ];
 
   const urbanRuralOptions = [
