36688638e26759604949eac1da435aa1d81151aa
Chavdar Shishkov <chavdar.shishkov123@gmail.com>
2023-10-20 14:57:56 +0300

    Refactor: Logout on reload/close tab functionality

--- src/blocks/Login/Login.jsx
+++ src/blocks/Login/Login.jsx
@@ -57,6 +57,7 @@ export const Login = () => {
       localStorage.setItem("token", token);
       localStorage.setItem("token-expires-in", expiresIn);
       localStorage.setItem("refresh-token", refreshToken);
+      sessionStorage.setItem("userSession", true);
 
       queryClient.setQueryData(
         ["client-data"],
@@ -90,7 +91,7 @@ export const Login = () => {
     loginMutation.mutate();
   };
 
-  const handleForgotPassowrd = () => {
+  const handleForgotPassword = () => {
     navigate("/forgot-password");
   };
 
@@ -125,7 +126,7 @@ export const Login = () => {
               color="purple"
               classes="login__grid__forgot-password"
               label={t("forgot_password_label")}
-              onClick={() => handleForgotPassowrd()}
+              onClick={() => handleForgotPassword()}
             />
             {errors.submit ? <Error message={errors.submit} /> : null}
             <Button
