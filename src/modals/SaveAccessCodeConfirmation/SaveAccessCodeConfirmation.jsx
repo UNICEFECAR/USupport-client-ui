b6af05027f476cb23fb2fd48f5295c526a069f70
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-12-22 13:00:01 +0200

    Refactor: SaveAccessTokenCodeConfirmation styling on dark mode

--- src/modals/SaveAccessCodeConfirmation/SaveAccessCodeConfirmation.jsx
+++ src/modals/SaveAccessCodeConfirmation/SaveAccessCodeConfirmation.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useTranslation } from "react-i18next";
 import {
   AccessToken,
   Modal,
   Icon,
   CheckBox,
 } from "@USupport-components-library/src";
+import { ThemeContext } from "@USupport-components-library/utils";
 
 import "./save-access-code-confirmation.scss";
-import { useTranslation } from "react-i18next";
 
 /**
  * SaveAccessCodeConfirmation
@@ -25,6 +26,8 @@ export const SaveAccessCodeConfirmation = ({
   showToast,
 }) => {
   const { t } = useTranslation("save-access-code-confirmation");
+  const { theme } = useContext(ThemeContext);
+
   const [hasAgreed, setHasAgreed] = useState(false);
   const [hasCopied, setHasCopied] = useState(false);
   const [shouldAnimate, setShouldAnimate] = useState(false);
@@ -61,6 +64,10 @@ export const SaveAccessCodeConfirmation = ({
         activateAnimation={shouldAnimate}
         copyLabel={t("click_to_copy")}
         showInstructions
+        classes={
+          theme === "dark" &&
+          "save-access-code-confirmation__access-token--dark"
+        }
       />
       <div className="save-access-code-confirmation__container">
         <Icon name="warning" size="md" />
