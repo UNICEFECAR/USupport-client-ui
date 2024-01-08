131d4a3f3d3bee5ff206b63240687826707bd839
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2024-01-08 17:03:35 +0200

    Add: GitBook URL to the environment variables

--- src/blocks/UserProfile/UserProfile.jsx
+++ src/blocks/UserProfile/UserProfile.jsx
@@ -12,6 +12,7 @@ import { useGetClientData } from "#hooks";
 import "./user-profile.scss";
 
 const AMAZON_S3_BUCKET = `${import.meta.env.VITE_AMAZON_S3_BUCKET}`;
+const GIT_BOOK_URL = `${import.meta.env.VITE_GIT_BOOK_URL}`;
 
 /**
  * UserProfile
@@ -137,12 +138,7 @@ export const UserProfile = ({ openModal, isTmpUser }) => {
             label={t("user_guide")}
             iconName="document"
             classes="user-profile__grid__item__button"
-            onClick={() =>
-              window.open(
-                "https://7digit-1.gitbook.io/usupport/y0yJCW2nZ6Sb52p4arjv",
-                "_blank"
-              )
-            }
+            onClick={() => window.open(GIT_BOOK_URL, "_blank")}
           />
           <ButtonSelector
             label={t("FAQ_button_label")}
