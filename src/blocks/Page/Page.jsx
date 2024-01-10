b1dd8b2fe50cfb7c9ad1120a56c9eb850ac140ba
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2024-01-10 11:26:41 +0200

    Add: contact us link to the footer props in Page block

--- src/blocks/Page/Page.jsx
+++ src/blocks/Page/Page.jsx
@@ -438,6 +438,7 @@ export const Page = ({
         <Footer
           lists={footerLists}
           contactUsText={t("contact_us")}
+          contactUsUrl="/contact-us"
           navigate={navigateTo}
           Link={Link}
           showSocials={false}
