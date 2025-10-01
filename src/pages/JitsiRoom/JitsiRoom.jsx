5707f99ae9a1255fda454c58c24eac22390696fa
7Digit-main <it-support@7digit.io>
2025-10-01 12:36:54 +0300

    Fix: search funcionality in Chat

--- src/pages/JitsiRoom/JitsiRoom.jsx
+++ src/pages/JitsiRoom/JitsiRoom.jsx
@@ -494,6 +494,7 @@ export const Chat = ({
   const [showOptions, setShowOptions] = useState(false);
   const [showAllMessages, setShowAllMessages] = useState(false);
   const [areSystemMessagesShown, setAreSystemMessagesShown] = useState(true);
+  const [search, setSearch] = useState("");
 
   // Handle message visibility
   useEffect(() => {
@@ -584,6 +585,8 @@ export const Chat = ({
       setShowAllMessages={setShowAllMessages}
       onTextareaFocus={handleTextareaFocus}
       emitTyping={emitTyping}
+      search={search}
+      setSearch={setSearch}
       isChatShownOnTablet={isChatShownOnTablet}
       isChatShownOnMobile={isChatShownOnMobile}
       setIsChatShownOnMobile={setIsChatShownOnMobile}
