612c8290967e146027ddd9a501c5b93719923166
7Digit-main <it-support@7digit.io>
2025-12-12 14:42:31 +0200

    Create: HowItWorksBA modal

--- src/blocks/BaselineAssessmentDashboard/BaselineAssessmentDashboard.jsx
+++ src/blocks/BaselineAssessmentDashboard/BaselineAssessmentDashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useCustomNavigate as useNavigate } from "#hooks";
 
@@ -7,10 +7,9 @@ import {
   Button,
   Box,
   Block,
-  Grid,
-  GridItem,
   Loading,
 } from "@USupport-components-library/src";
+import { HowItWorksBA } from "#modals";
 
 import { useGetLatestBaselineAssessment } from "#hooks";
 
@@ -27,6 +26,8 @@ export const BaselineAssessmentDashboard = ({
   openBaselineAssesmentModal,
   isTmpUser,
 }) => {
+  const [isHowItWorksBAOpen, setIsHowItWorksBAOpen] = useState(false);
+
   const navigate = useNavigate();
   const { t } = useTranslation("blocks", {
     keyPrefix: "baseline-assessment-dashboard",
@@ -42,64 +43,78 @@ export const BaselineAssessmentDashboard = ({
   };
 
   return (
-    <Block classes="baseline-assessment-dashboard">
-      <div className="baseline-assessment-dashboard__content">
-        <h4 className="baseline-assessment-dashboard__heading">
-          {t("heading")}
-        </h4>
+    <React.Fragment>
+      <HowItWorksBA
+        isOpen={isHowItWorksBAOpen}
+        onClose={() => setIsHowItWorksBAOpen(false)}
+      />
+      <Block classes="baseline-assessment-dashboard">
+        <div className="baseline-assessment-dashboard__content">
+          <div className="mood-tracker__heading">
+            <h4 className="baseline-assessment-dashboard__heading">
+              {t("heading")}
+            </h4>
+            <p
+              className="small-text mood-tracker-button"
+              onClick={() => setIsHowItWorksBAOpen(true)}
+            >
+              {t("how_it_works")}
+            </p>
+          </div>
 
-        <div className="baseline-assessment-dashboard__assessment">
-          {isFetching ? (
-            <Loading />
-          ) : !latestAssessment ? (
-            <Button size="lg" onClick={openBaselineAssesmentModal}>
-              {t("start_new_assessment")}
-            </Button>
-          ) : latestAssessment.status === "completed" ? (
-            <div className="baseline-assessment-dashboard__results">
-              <h5 className="baseline-assessment-dashboard__results__heading">
-                {t("latest_results")}
-              </h5>
-              <div className="baseline-assessment-dashboard__results__items">
-                <Box classes="baseline-assesment-result__compare-grid__item">
-                  <p>
-                    {t("psychological")}:{" "}
-                    {latestAssessment.finalResult.psychologicalScore}
-                  </p>
-                </Box>
-                <Box classes="baseline-assesment-result__compare-grid__item">
-                  <p>
-                    {t("social")}: {latestAssessment.finalResult.socialScore}
-                  </p>
-                </Box>
-                <Box classes="baseline-assesment-result__compare-grid__item">
-                  <p>
-                    {t("biological")}:{" "}
-                    {latestAssessment.finalResult.biologicalScore}
-                  </p>
-                </Box>
-              </div>
-              <Button
-                classes="baseline-assessment-dashboard__results__button"
-                size="lg"
-                onClick={openBaselineAssesmentModal}
-              >
+          <div className="baseline-assessment-dashboard__assessment">
+            {isFetching ? (
+              <Loading />
+            ) : !latestAssessment ? (
+              <Button size="lg" onClick={openBaselineAssesmentModal}>
                 {t("start_new_assessment")}
               </Button>
-            </div>
-          ) : (
-            <BaselineAssesmentBox
-              progress={latestAssessment.completionPercentage}
-              status={latestAssessment.status}
-              startedAt={latestAssessment.startedAt}
-              currentPosition={latestAssessment.currentPosition}
-              completionPercentage={latestAssessment.completionPercentage}
-              handleViewAssessment={handleViewAssessment}
-              t={t}
-            />
-          )}
+            ) : latestAssessment.status === "completed" ? (
+              <div className="baseline-assessment-dashboard__results">
+                <h5 className="baseline-assessment-dashboard__results__heading">
+                  {t("latest_results")}
+                </h5>
+                <div className="baseline-assessment-dashboard__results__items">
+                  <Box classes="baseline-assesment-result__compare-grid__item">
+                    <p>
+                      {t("psychological")}:{" "}
+                      {latestAssessment.finalResult.psychologicalScore}
+                    </p>
+                  </Box>
+                  <Box classes="baseline-assesment-result__compare-grid__item">
+                    <p>
+                      {t("social")}: {latestAssessment.finalResult.socialScore}
+                    </p>
+                  </Box>
+                  <Box classes="baseline-assesment-result__compare-grid__item">
+                    <p>
+                      {t("biological")}:{" "}
+                      {latestAssessment.finalResult.biologicalScore}
+                    </p>
+                  </Box>
+                </div>
+                <Button
+                  classes="baseline-assessment-dashboard__results__button"
+                  size="lg"
+                  onClick={openBaselineAssesmentModal}
+                >
+                  {t("start_new_assessment")}
+                </Button>
+              </div>
+            ) : (
+              <BaselineAssesmentBox
+                progress={latestAssessment.completionPercentage}
+                status={latestAssessment.status}
+                startedAt={latestAssessment.startedAt}
+                currentPosition={latestAssessment.currentPosition}
+                completionPercentage={latestAssessment.completionPercentage}
+                handleViewAssessment={handleViewAssessment}
+                t={t}
+              />
+            )}
+          </div>
         </div>
-      </div>
-    </Block>
+      </Block>
+    </React.Fragment>
   );
 };
