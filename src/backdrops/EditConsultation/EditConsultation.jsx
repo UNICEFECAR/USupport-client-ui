4840d973ef7327829919d26384c6fd113cb26c54
7Digit-Main <it-support@7digit.io>
2026-01-23 11:18:20 +0200

    Refactor: hide the edit consultation button if less than 24 hours before in EditConsultation.jsx

--- src/backdrops/EditConsultation/EditConsultation.jsx
+++ src/backdrops/EditConsultation/EditConsultation.jsx
@@ -30,8 +30,12 @@ export const EditConsultation = ({
   const imageUrl = image || "default";
   const startDate = new Date(timestamp);
   const endDate = new Date(timestamp + ONE_HOUR);
+  const today = new Date().getTime();
 
   const { t } = useTranslation("backdrops", { keyPrefix: "edit-consultation" });
+
+
+  const startsInLessThan24Hours = (startDate - today) < 24 * 60 * 60 * 1000;
 
   const handleCancelClick = () => {
     onClose();
@@ -60,12 +64,12 @@ export const EditConsultation = ({
         classes="edit-consultation__provider-consultation"
         t={t}
       />
-      <ButtonSelector
+    {!startsInLessThan24Hours &&      <ButtonSelector
         onClick={handleEditClick}
         iconName="calendar"
         label={t("date_button_label")}
         classes="edit-consultation__button"
-      />
+      />}
       <ButtonSelector
         onClick={handleCancelClick}
         iconName="close-x"
