9e3c014c7296f663d524113c9f9a96fa06be3f4a
7Digit-main <it-support@7digit.io>
2026-06-26 12:06:47 +0300

    Add: Integrate client user guide link in Page component and update UserProfile button to open the guide in a new tab

--- src/blocks/UserProfile/UserProfile.jsx
+++ src/blocks/UserProfile/UserProfile.jsx
@@ -149,7 +149,9 @@ export const UserProfile = ({ openModal, isTmpUser }) => {
             label={t("user_guide")}
             iconName="document"
             classes="user-profile__grid__item__button"
-            onClick={() => window.open(GIT_BOOK_URL, "_blank")}
+            onClick={() =>
+              window.open(`${GIT_BOOK_URL}/ui-usage-manuals/client`, "_blank")
+            }
           />
           <ButtonSelector
             label={t("FAQ_button_label")}
