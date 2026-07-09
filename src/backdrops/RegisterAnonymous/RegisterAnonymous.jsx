997e3575ac13c23499c37fd990957cce42b37794
7Digit-main <it-support@7digit.io>
2026-07-09 13:48:36 +0300

    Add: Set 'isRegistered' flag in localStorage upon successful registration in CodeVerification and RegisterAnonymous components

--- src/backdrops/RegisterAnonymous/RegisterAnonymous.jsx
+++ src/backdrops/RegisterAnonymous/RegisterAnonymous.jsx
@@ -111,6 +111,7 @@ export const RegisterAnonymous = ({
       localStorage.setItem("token", token);
       localStorage.setItem("token-expires-in", expiresIn);
       localStorage.setItem("refresh-token", refreshToken);
+      localStorage.setItem("isRegistered", "true");
 
       window.dispatchEvent(new Event("login"));
       window.dispatchEvent(new Event("token-changed"));
